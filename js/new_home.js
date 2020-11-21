b73685b968076348a0b927029d17ca4145011a1b
Swapnaneel Bhuiya <swapnaneel@Swapnaneels-MacBook-Air.local>
2020-11-21 20:08:18 +0530

    [Swapnaneel]ADDED: Updated Files

--- js/new_home.js
+++ js/new_home.js
@@ -3,7 +3,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     empList = getEmployeePayrollDataFromStorage();
     document.querySelector(".emp-count").textContent = empList.length;
     createInnerHtml();
-})
+    // localStorage.removeItem('editEmp');
+});
 
 const getEmployeePayrollDataFromStorage = () => {
     return localStorage.getItem("EmployeePayrollList") ? JSON.parse(localStorage.getItem("EmployeePayrollList")) : [];
@@ -11,34 +12,28 @@ const getEmployeePayrollDataFromStorage = () => {
 
 
 const createInnerHtml = () => {
+    let innerHtml='';
     if(empList.length==0)return;
-    const headerHtml = `<tr>
-    <th></th>
-    <th>Name</th>
-    <th>Gender</th>
-    <th>Department</th>
-    <th>Salary</th>
-    <th>Start Date</th>
-    <th>Actions</th>
-  </tr>`;
-  let innerHtml = ` ${headerHtml}`;
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+    "<th>Salary</th><th>Start Date</th><th>Note</th>";
+  if(empList.length>0) innerHtml = ` ${headerHtml}`;
   for (const employee of empList) {
       innerHtml = `${innerHtml}
       <tr>
-        <td><img class="profile" src="${employee._profile}" alt = ""></td>
+        <td><img class="profile" src="${employee._profilePic}" alt = ""></td>
         <td>${employee._name}</td>
         <td>${employee._gender}</td>
         <td>${getDeptHtml(employee._department)}</td>
         <td>${employee._salary}</td>
-        <td>${employee._startDate}</td>
+        <td>${stringifyDate(employee._startDate)}</td>
         <td>
-        <img name="${employee._name}" onclick="remove(this)" alt="delete" 
+        <img name="${employee._id}" onclick="remove(this)" alt="delete" 
                 src="/assets/assets/icons/delete-black-18dp.svg">
-        <img name="${employee._name}" onclick="update(this)" alt="edit" 
+        <img name="${employee._id}" onclick="update(this)" alt="edit" 
                 src="/assets/assets/icons/create-black-18dp.svg">
         </td>
       </tr>`;
-  };
+  }
     document.querySelector('#table-display').innerHTML = innerHtml;
 }  
 const createEmployeePayrollJSON = () => {
@@ -49,20 +44,20 @@ const createEmployeePayrollJSON = () => {
             "_department": [
                 "Engineering"
             ],
-            "_salary": "400000",
+            "_salary": "450000",
             "_startDate": "16 Dec 2018",
-            "_notes": "",
+            "_note": "",
             "_profile": "/assets/assets/profile-images/Ellipse -2.png"
         },
         {
             "_name": "Sourav Das",
-            "_gender": "Female",
+            "_gender": "Male",
             "_department": [
                 "Sales", "Marketing"
             ],
             "_salary": "500000",
             "_startDate": "19 Sept 2020",
-            "_notes": "",
+            "_note": "",
             "_profile": "/assets/assets/profile-images/Ellipse -1.png"
         }
     ]
